adcc3a6468ff9f30c35047d2c0bf94b9e06e748a
HSU CHIA-CHI <86188415+CHIACHISMILE@users.noreply.github.com>
2026-02-12 01:05:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tripapp-v1";
+const CACHE = "tripapp-v1.1";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
